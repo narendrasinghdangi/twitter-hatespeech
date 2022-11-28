ca59c8b2a620cf8e460b881741a78f66dd9aa84a
Narendra Singh dangi <ns172128561@gmail.com>
2022-11-28 23:55:58 +0530

    add some

--- main.py
+++ main.py
@@ -44,20 +44,16 @@ async def read_items(q: str | None = Query(default=None, max_length=100)):
     data['tl'] = [''.join([WordNetLemmatizer().lemmatize(
         re.sub('[^A-Za-z]', ' ', text)) for text in li]) for li in data['tweet']]
 
-    a = []
+    cleaned_twwet = []
     for text in data['tl']:
         word_list = word_tokenize(text)
         text = filter_words(word_list)
-        a.append(text)
+        sent = ""
+        for word in text:
+            sent += str(word) + ' '
+        cleaned_twwet.append(sent)
 
-    train_text = []
-    for i in a:
-        sent = ''
-        for j in i:
-            sent += str(j) + ' '
-        train_text.append(sent)
-
-    data['cleaned_tweets'] = train_text
+    data['cleaned_tweets'] = cleaned_twwet
 
     # Transforming our data using the vector trained on training data.
     vectorized_tweets = vect.transform(data['cleaned_tweets'])
